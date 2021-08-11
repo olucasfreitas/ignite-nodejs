4c8af648e87ec735f9f94b82fe828038cb41db21
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-08-11 20:15:02 -0300

    Validando CPF existente

--- primeiro-projeto-node/src/index.js
+++ primeiro-projeto-node/src/index.js
@@ -9,13 +9,21 @@ const customers = [];
 
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
-  const id = uuidv4();
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: "This customer already exists." });
+  }
+
   customers.push({
-    id,
+    id: uuidv4(),
     cpf,
     name,
     statement: [],
   });
+  
   return response.status(201).send();
 });
 
