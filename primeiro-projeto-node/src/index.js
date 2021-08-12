d2ce2d8a0f777ade9267bed4a2f6fc0c4e28a8e8
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-08-11 21:46:44 -0300

    Middlewares

--- primeiro-projeto-node/src/index.js
+++ primeiro-projeto-node/src/index.js
@@ -7,6 +7,20 @@ app.use(express.json());
 
 const customers = [];
 
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf == cpf);
+
+  if (!customer) {
+    return response.status(400).json({ error: "This customer was not found." });
+  }
+
+  request.customer = customer;
+
+  return next();
+}
+
 app.post("/account", (request, response) => {
   const { cpf, name } = request.body;
   const customerAlreadyExists = customers.some(
@@ -29,17 +43,10 @@ app.post("/account", (request, response) => {
   return response.status(201).send();
 });
 
-app.get("/statement", (request, response) => {
-  const { cpf } = request.headers;
+// app.use(verifyIfExistsAccountCPF);
 
-  const customer = customers.find((customer) => customer.cpf == cpf);
-
-  if(!customer){
-    return response
-      .status(400)
-      .json({ error: "This customer was not found." });
-  } 
-  
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
   return response.json(customer.statement);
 });
 
