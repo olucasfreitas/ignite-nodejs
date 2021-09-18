3aa0b0f146bc2eccbea3e39de556408279e83a3f
Lucas Freitas <lucasdasilvafreitas090@gmail.com>
2021-09-18 16:10:38 -0300

    Criando repositório de especificação

--- 3 - iniciando-api/src/modules/cars/services/CreateSpecificationService.ts
+++ 3 - iniciando-api/src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,21 @@
+import { ISpecificationRepository } from "../repositories/ISpecificationsRepository";
+
+interface IRequest {
+  name: string;
+  description: string;
+}
+
 class CreateSpecificationService {
-  execute() {
-    console.log("");
+  constructor(private specificationRepository: ISpecificationRepository) {}
+  execute({ name, description }: IRequest): void {
+    const specificationAlreadyExists =
+      this.specificationRepository.findByName(name);
+
+    if (specificationAlreadyExists) {
+      throw new Error("Specification already exists");
+    }
+
+    this.specificationRepository.create({ name, description });
   }
 }
 
